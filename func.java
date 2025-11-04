c09bc584816c652b5146eb206cb01e2a157b6528
Om Kumar <114673166+Omkumar2003@users.noreply.github.com>
2025-11-04 15:22:32 +0530

    Enhance comments on Thread usage in func.java
    Added comments explaining the usage of the Thread start() method.

--- func.java
+++ func.java
@@ -1,3 +1,8 @@
+// Thread start() method is like a condom ..........can only be use once ..if u try to make that i throws exception
+//to use thread multiple time ....u can to create it inside the loop then start it ...in every iteration it will generate the new thread
+
+
+
 class Main{
     public static void main(String[] args){
         A a= new A(()->{
